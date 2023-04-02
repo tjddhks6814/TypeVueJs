22328fb73dde9ef1d07feac914eba42cd8e2a1a1
tjddhks6814 <tjddhks6814@naver.com>
2023-04-02 17:51:22 +0900

    Login test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>{{count}}</div>
+    <button @click="increment()">증가</button><br>
+    <input type="text" name="userId" v-model='state.form.userId'><br>
+    <input type="password" name="userPw" v-model='state.form.userPw'><br>
+    <button @click="submit()">로그인</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import router from '@/router'
+import { reactive } from 'vue'
 
-@Options({
-  components: {
-    HelloWorld
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  setup () {
+    const state = reactive({
+      form: {
+        userId: '',
+        userPw: ''
+      }
+    })
+
+    const submit = () => {
+      if (state.form.userId === '') {
+        alert('ID를 입력해주새요')
+        return false
+      }
+      if (state.form.userPw === '') {
+        alert('PW를 입력해주새요')
+        return false
+      }
+      router.push('/about')
+      alert('로그인 되었습니다')
+    }
+    return { state, submit }
+  },
+  methods: {
+    increment () {
+      this.count++
+    }
   }
-})
-export default class HomeView extends Vue {}
+}
 </script>
